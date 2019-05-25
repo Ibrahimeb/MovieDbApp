a8d3f996ef4ae0cb65d6a83e88acf7a791c40e72
ibrahim estanga <Ibrahimeb3011@gmail.com>
2019-05-25 18:35:50 -0400

    update

--- app/src/main/java/com/ibrahim/moviedbapp/home/tvShow/di/TvshowModule.kt
+++ app/src/main/java/com/ibrahim/moviedbapp/home/tvShow/di/TvshowModule.kt
@@ -1,2 +1,26 @@
 package com.ibrahim.moviedbapp.home.tvShow.di
 
+import com.ibrahim.moviedbapp.home.tvShow.api.TvShowApi
+import com.ibrahim.moviedbapp.home.tvShow.mvp.TvShowContract
+import com.ibrahim.moviedbapp.home.tvShow.mvp.TvShowModel
+import com.ibrahim.moviedbapp.home.tvShow.mvp.TvShowPresenter
+import dagger.Module
+import dagger.Provides
+
+@Module
+class TvshowModule(val view:TvShowContract.View){
+
+    @Provides
+    @TvShowScope
+    fun providerView() = view
+
+    @Provides
+    @TvShowScope
+    fun providerModel(api:TvShowApi) = TvShowModel(api)
+
+    @Provides
+    @TvShowScope
+    fun providerPresenter(view:TvShowContract.View,model: TvShowModel)=TvShowPresenter(view,model)
+
+
+}
