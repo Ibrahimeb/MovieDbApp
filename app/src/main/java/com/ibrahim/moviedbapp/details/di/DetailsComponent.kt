84e9b9fa0e1abfee9b24a76b2dca73d28cab7908
ibrahim estanga <Ibrahimeb3011@gmail.com>
2019-05-26 15:21:03 -0400

    update

--- app/src/main/java/com/ibrahim/moviedbapp/details/di/DetailsComponent.kt
+++ app/src/main/java/com/ibrahim/moviedbapp/details/di/DetailsComponent.kt
@@ -1,4 +1,14 @@
 package com.ibrahim.moviedbapp.details.di
 
+import com.ibrahim.moviedbapp.details.ui.DetailsMovieFragment
+import dagger.Component
+import dagger.Subcomponent
+
+@DetailScope
+@Subcomponent(modules = [DetailsModule::class])
 interface DetailsComponent {
+
+    fun inject(detailsMovieFragment: DetailsMovieFragment)
+
+
 }
